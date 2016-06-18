0f2d3ad48107009a27676a0f76d36bfc57aa419f
weixin <wuweixin6@gmail.com>
2016-06-18 17:42:02 -0400

    add service

--- app/src/main/java/com/weixin/xplayer/MainActivity.java
+++ app/src/main/java/com/weixin/xplayer/MainActivity.java
@@ -1,14 +1,24 @@
 package com.weixin.xplayer;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -22,6 +32,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.io.File;
@@ -32,27 +44,46 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Random;
 
+import static java.lang.System.currentTimeMillis;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView songList;
-    ArrayList<File> songFiles;
+    ArrayList<MusicFile> songFiles;
     ArrayList<File> songFilesRandom;
     ArrayList<String> arrayList;
     ImageButton btn_play;
+    Button btn_scan;
+    SeekBar seekBar;
     int currentPlaying;
     final MediaPlayer mediaPlayer  = new MediaPlayer();
     boolean isRandom = false;
+    SharedPreferences sharedpreference;
+    SharedPreferences.Editor editor ;
+    final String  sharedPrefsSongName = "songName";
+    final String sharedPrefsSongPath = "songPath";
+    final String sharedPrefsSongCount = "sharedPrefsSongCount";
+    final String sharedPrefsIsLoaded = "sharedPrefsIsLoaded";
+    ProgressDialog progressdialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
 
+        btn_scan=(Button)findViewById(R.id.scanMusic);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         btn_play = (ImageButton)findViewById(R.id.button_play);
         currentPlaying = -1;
+
+
+        sharedpreference = getSharedPreferences("musicInfo", Context.MODE_PRIVATE);
+
+        editor = sharedpreference.edit();
+        editor.clear().commit();
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,18 +113,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
         songList = (ListView)findViewById(R.id.songList);
-        songFiles= new ArrayList<File>();
-        songFilesRandom = new ArrayList<File>(songFiles);
+        songFiles= new ArrayList<MusicFile>();
+        //songFilesRandom = new ArrayList<File>(songFiles);
         arrayList = new ArrayList<String>();
+        songFiles.clear();
+        arrayList.clear();
+        //check if all the song is loaded previously
+        if (!(sharedpreference.getBoolean(sharedPrefsIsLoaded,false))){
+            btn_scan.setVisibility(View.VISIBLE);
 
-        File musicFolder = null;
-        musicFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
-        File[] p = musicFolder.listFiles();
-        getSong(p);
-        ArrayAdapter adapter = new ArrayAdapter<String>(getBaseContext(),android.R.layout.simple_list_item_1,arrayList);
-        songList.setAdapter(adapter);
-
-
+        }else {
+            btn_scan.setVisibility(View.INVISIBLE);
+            File f = null;
+            new scanForMusic().execute(f);
+        }
 
 
         songList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -102,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
 
                     if (mediaPlayer.isPlaying()) {
-                       System.out.println("++++++++++++++++++++++ is playing");
+
                         mediaPlayer.stop();
                         mediaPlayer.reset();
                     }else {
@@ -111,8 +144,8 @@ public class MainActivity extends AppCompatActivity {
                     String path = songFiles.get(position).getPath();
                     currentPlaying = position;
                     mediaPlayer.setDataSource(path);
+                    //if (mediaPlayer.getDuration()<40000) System.out.print("----------------------");
                     mediaPlayer.prepareAsync();
-
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -120,26 +153,116 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(),R.drawable.ic_play_arrow_black_48dp);
+        Notification noti = new Notification.Builder(getBaseContext())
+                .setContentTitle("New mail from " + "Asdasd".toString())
+                .setContentText("context")
+                .setSmallIcon(R.drawable.ic_pause_black_48dp)
+                .setLargeIcon(bmp)
+                .build();
 
+
+
+    }
+    public void scanSongOnClick(View v){
+        File musicFolder = null;
+        musicFolder = Environment.getExternalStorageDirectory();
+        File[] p = musicFolder.listFiles();
+        new scanForMusic().execute(p);
     }
     private void getSong(File[] f){
         if (f !=null){
             int i = 0;
             for (i=0; i<f.length;i++){
-                getSong(f[i].listFiles());
+                String fName = f[i].getName();
+                if (!fName.substring(0,1).equals(".") && !fName.equals("backups")
+                        && (!fName.equals("Android")) && (!fName.equals("DCIM")))
+                    getSong(f[i].listFiles());
+
+
             }
             for (int j = 0 ; j < f.length;j++) {
-                if (f[j].getName().contains("mp3")) {
-                    songFiles.add(f[j]);
-                    arrayList.add(f[j].getName());
 
+                if (f[j].getName().contains("mp3")) {
+                    String name = f[j].getName();
+                    String songTitle;
+                    if (name.substring(name.length() - 4, name.length()).equals(".mp3")) {
+                        String mediaPath = f[j].getAbsolutePath();
+                        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+                        mmr.setDataSource(mediaPath);
+                        String duration = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+                        String title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+                        String artist = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+                        if (title ==null || artist == null) {
+                            title = name.substring(0, name.length() - 4);
+                        }
+                        mmr.release();
+                        if (Integer.valueOf(duration) > 30000){
+                            MusicFile musicFile = new MusicFile(f[j].getName(), f[j].getPath(), f[j]);
+                            musicFile.setTitle(title);
+                            musicFile.setArtist(artist);
+                            songFiles.add(musicFile);
+                            arrayList.add(musicFile.getSongTitle());
+                        }
+                    }
                 }
             }
+
         }
     }
+
+    private class scanForMusic extends AsyncTask<File,Void,Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressdialog = ProgressDialog.show(MainActivity.this,"Loading","");
+
+        }
+
+        @Override
+        protected Void doInBackground(File... params) {
+
+            if (!(sharedpreference.getBoolean(sharedPrefsIsLoaded,false))) {
+                sharedpreference.edit().putBoolean(sharedPrefsIsLoaded,true).commit();
+                getSong(params);
+                int length = songFiles.size();
+                editor.putInt(sharedPrefsSongCount, length);
+                for (int i = 0; i < length; i++) {
+                    editor.putString(sharedPrefsSongName + i, songFiles.get(i).getSongTitle());
+                    editor.putString(sharedPrefsSongPath + i, songFiles.get(i).getPath());
+                }
+                editor.commit();
+
+            }else {
+
+                int length = sharedpreference.getInt(sharedPrefsSongCount,-1);
+                if (length>0){
+                    for (int i = 0 ; i < length; i++){
+                        String name = sharedpreference.getString(sharedPrefsSongName+i,"not found");
+                        String path = sharedpreference.getString(sharedPrefsSongPath+i,"not found");
+                        File f = new File(sharedpreference.getString(sharedPrefsSongPath+i,"not found"));
+                        songFiles.add(new MusicFile(name,path,f));
+                        arrayList.add(name);
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void Void) {
+            ArrayAdapter adapter = new ArrayAdapter<String>(getBaseContext(),android.R.layout.simple_list_item_1,arrayList);
+            songList.setAdapter(adapter);
+            songList.setVisibility(View.VISIBLE);
+            btn_scan.setVisibility(View.INVISIBLE);
+            progressdialog.dismiss();
+        }
+
+    }
+
     public void onClickPlay(View v) throws IOException {
-        if (mediaPlayer.getDuration()<20) {
-            System.out.println("++++++++++++++++"+"in the duration lt 20 " + mediaPlayer.getDuration());
+        if (mediaPlayer.getDuration()<60000) {
+            System.out.println("--------"+"in the duration lt 20 " + mediaPlayer.getDuration());
             mediaPlayer.reset();
             currentPlaying = 0;
             String path = songFiles.get(0).getPath();
--- app/src/main/java/com/weixin/xplayer/PlayingView.java
+++ app/src/main/java/com/weixin/xplayer/PlayingView.java
@@ -18,4 +18,6 @@ public class PlayingView extends AppCompatActivity {
     }
     //c
 
+
+
 }
